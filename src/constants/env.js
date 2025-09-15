24889717e194c0c34ad15efdbb97f6e6fcb446c1
hoanghung256 <hoanghung250604@gmail.com>
2025-09-16 00:53:05 +0700

    fix import error

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,3 +1,4 @@
-export const BE_BASE_URL = import.meta.VITE_APP_BE_BASE_URL;
+export const BE_BASE_URL = import.meta.env.VITE_APP_BE_BASE_URL;
 
-export const API_KEY = import.meta.VITE_APP_API_KEY;
+export const API_KEY = import.meta.env.VITE_APP_API_KEY;
+    
--- src/utils/apiConnector.js
+++ src/utils/apiConnector.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { log } from "log";
+import { log } from "../utils/log";
 import { HTTP_RESPONSE_STATUS_CODE } from "../constants/api";
 
 export const axiosInstance = axios.create({});
