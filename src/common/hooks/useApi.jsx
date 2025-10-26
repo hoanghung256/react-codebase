b180b6206e778149eff4f5328e0e43acbee37c0c
hoanghung256 <hoanghung250604@gmail.com>
2025-10-26 15:42:15 +0700

    feat(hook): add custom hook 'useApi'

---
+++ src/common/hooks/useApi.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useMemo, useState } from "react";
+import { apiConnector, axiosInstance } from "../utils/apiConnector";
+import { HTTP_RESPONSE_STATUS_CODE, METHOD } from "../constants/api";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+function useApi({ method, apiEndpoint, displaySuccessMessage = false, arg = null }) {
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const argKey = useMemo(() => JSON.stringify(arg), [arg]);
+
+    useEffect(() => {
+        let isMounted = true;
+        if (method && apiEndpoint && displaySuccessMessage) {
+            callApi(isMounted);
+        }
+        return () => {
+            isMounted = false;
+        };
+    }, [method, apiEndpoint, displaySuccessMessage, argKey]);
+
+    const callApi = async (isMounted) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const isGetOrDelete = method === METHOD.GET || method === METHOD.DELETE;
+            const response = await axiosInstance({
+                method,
+                url: apiEndpoint,
+                data: !isGetOrDelete ? arg : null,
+                params: isGetOrDelete ? arg : null,
+            });
+
+            if (isMounted) {
+                if (!response.data.success) {
+                    throw new Error(response.data.message);
+                }
+                if (displaySuccessMessage) {
+                    toast.success(response.data.message || "Successful");
+                }
+                setData(response.data.data);
+            }
+        } catch (error) {
+            if (error.status == HTTP_RESPONSE_STATUS_CODE.UNAUTHORIZED) {
+                confirm("Unauthorized access. Please log in again.");
+                localStorage.removeItem("token");
+                localStorage.removeItem("user");
+                navigate("/login");
+            }
+            if (isMounted) {
+                setError(error);
+            }
+        } finally {
+            if (isMounted) {
+                setLoading(false);
+            }
+        }
+    };
+
+    return { data, error, loading };
+}
+
+export default useApi;
--- src/common/utils/apiConnector.js
+++ src/common/utils/apiConnector.js
@@ -1,31 +1,13 @@
 import axios from "axios";
-import { log } from "../utils/log";
-import { HTTP_RESPONSE_STATUS_CODE } from "../constants/api";
+import { BE_BASE_URL } from "../constants/env";
 
-export const axiosInstance = axios.create({});
+export const axiosInstance = axios.create({
+    baseURL: BE_BASE_URL,
+});
 
-export const apiConnector = (method, url, bodyData, headers, params) => {
-    const res = axiosInstance({
-        method: `${method}`,
-        url: `${url}`,
-        data: bodyData ? bodyData : null,
-        headers: headers ? headers : null,
-        params: params ? params : null,
-    });
+axiosInstance.interceptors.response.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
 
-    res.then((data) => {
-        log(data);
-    }).catch((error) => {
-        if (error.status == HTTP_RESPONSE_STATUS_CODE.UNAUTHORIZED) {
-            log("Unauthorized");
-            // dispatch(setToken(null));
-            // dispatch(setUser(null));
-            localStorage.removeItem("token");
-            localStorage.removeItem("user");
-            window.location.href = "/";
-        }
-        log(error);
-    });
-
-    return res;
-};
+    return config;
+});
