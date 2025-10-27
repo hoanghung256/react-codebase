1d892e9a85d757e1019921d41545bec04d5b602b
hoanghung256 <hoanghung250604@gmail.com>
2025-10-27 10:17:13 +0700

    feat(apiConnector): add util function callApi()

--- src/common/utils/apiConnector.js
+++ src/common/utils/apiConnector.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { BE_BASE_URL } from "../constants/env";
+import { HTTP_RESPONSE_STATUS_CODE, METHOD } from "../constants/api";
+import toast from "react-hot-toast";
+import { setLoading } from "../store/authSlice";
+import { store } from "../../main";
 
 export const axiosInstance = axios.create({
     baseURL: BE_BASE_URL,
@@ -11,3 +15,38 @@ axiosInstance.interceptors.response.use((config) => {
 
     return config;
 });
+
+export const callApi = async ({ method, endpoint, displaySuccessMessage = false, alertErrorMessage = false, arg }) => {
+    try {
+        store.dispatch(setLoading(true));
+        const isGetOrDelete = method === METHOD.GET || method === METHOD.DELETE;
+        const response = await axiosInstance({
+            method,
+            url: endpoint,
+            data: !isGetOrDelete ? arg : null,
+            params: isGetOrDelete ? arg : null,
+        });
+
+        if (!response.data.success) {
+            throw new Error(response.data.message);
+        }
+        if (displaySuccessMessage) {
+            toast.success(response.data.message || "Successful");
+        }
+        return response.data.data;
+    } catch (error) {
+        if (error.status == HTTP_RESPONSE_STATUS_CODE.UNAUTHORIZED) {
+            confirm("Unauthorized access. Please log in again.");
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            window.location.href = "/login";
+        }
+        if (alertErrorMessage) {
+            alert(error.message || "An error occurred");
+        } else {
+            throw error;
+        }
+    } finally {
+        store.dispatch(setLoading(false));
+    }
+};
