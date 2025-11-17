e334e7f76d989e5cf2478c157c7c2d802a9af7c4
hoanghung256 <hoanghung250604@gmail.com>
2025-11-17 20:48:27 +0700

    common(component): add component to select bank

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import useUser from "./common/hooks/useUser";
+import BankSelection from "./common/components/BankSelection";
+import { useState } from "react";
 
 function App() {
     const user = useUser();
+    const [bankId, setBankId] = useState(null);
 
     return (
         <div>
@@ -13,15 +16,28 @@ function App() {
                     Fetch your profile
                 </Button>
             )}
-            <Button variant="contained" color="secondary" LinkComponent={Link} to={"/interview"} style={{ marginLeft: 16 }}>
+            <Button
+                variant="contained"
+                color="secondary"
+                LinkComponent={Link}
+                to={"/interview"}
+                style={{ marginLeft: 16 }}
+            >
                 Interview Rooms
             </Button>
-            <Button variant="contained" color="secondary" LinkComponent={Link} to={"/signup"} style={{ marginLeft: 16 }}>
+            <Button
+                variant="contained"
+                color="secondary"
+                LinkComponent={Link}
+                to={"/signup"}
+                style={{ marginLeft: 16 }}
+            >
                 Signup
             </Button>
             <Button variant="contained" color="secondary" LinkComponent={Link} to={"/login"} style={{ marginLeft: 16 }}>
                 Login
             </Button>
+            <BankSelection bankId={bankId} onBankIdChange={setBankId} />
         </div>
     );
 }
