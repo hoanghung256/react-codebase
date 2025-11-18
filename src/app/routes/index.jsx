9b1551ed9db777d69451e5a31f76392d34a6755a
quocanh <quocanh.dev404@gmail.com>
2025-11-18 17:37:01 +0700

    feat: interviewer schedule screen to manage availabilities slots.

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -2,6 +2,7 @@ import App from "../../App";
 import InterviewRoomListPage from "../../features/interview/pages/InterviewRoomListPage/InterviewRoomListPage";
 import InterviewRoomPage from "../../features/interview/pages/InterviewRoomPage/InterviewRoomPage";
 import HomePage from "../../features/home/pages/HomePage";
+import ScheduleManagement from "../../features/interviewer/pages/ScheduleManagement";
 import AuthLayout from "../layouts/AuthLayout";
 import MainLayout from "../layouts/MainLayout";
 import { authRoutes } from "./authRoutes";
@@ -15,6 +16,7 @@ export const routes = [
             { path: "/test", element: <App /> },
             { path: "/interview", element: <InterviewRoomListPage /> },
             { path: "/interview/room/:roomId", element: <InterviewRoomPage /> },
+            { path: "/schedule", element: <ScheduleManagement /> },
         ],
     },
     {element: <MainLayout />, children: intervieweeRoutes}
